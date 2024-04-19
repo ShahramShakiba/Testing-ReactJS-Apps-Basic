dcd96ae35d81a05d249dc3f2212bb2f649f0b079
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-19 21:43:29 +0330

    Grouping Tests Together With Test Suites - describe()

--- Testing-Apps/src/components/Greeting.test.js
+++ Testing-Apps/src/components/Greeting.test.js
@@ -1,20 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
 
-// test(description: what your test does, fn: testing codes )
-test('renders Hello World as a text', () => {
-  // Arrange: set up test data
-  render(<Greeting />);
+describe('<Greeting />', () => {
+  // test(description: what your test does, fn: testing codes )
+  test('renders Hello World as a text', () => {
+    // Arrange: set up test data
+    render(<Greeting />);
 
-  // Act: run logic that should be tested
-  // ...
+    // Act: run logic that should be tested
+    // ...
 
-  // Assert: compare execution results with expected results
-  const helloWorldEl = screen.getByText('Hello World', { exact: false });
+    // Assert: compare execution results with expected results
+    const helloWorldEl = screen.getByText('Hello World', { exact: false });
 
-  expect(helloWorldEl).toBeInTheDocument(); // toBeInTheDocument() is a matcher
+    expect(helloWorldEl).toBeInTheDocument(); // toBeInTheDocument() is a matcher
+  });
 });
 
 /* matchers
 - a matcher function to define the assertion condition, leading to the error message "Expect must have a corresponding matcher call."
+*/
+
+/* Test Suites & Tests & describe
+* Test Suites: A test suite is a collection of individual test cases or test files that are grouped together based on a common functionality or feature.
+  - a test suite could include tests for components, functions, or specific features within the application.
+
+
+* Tests: on the other hand, refer to individual units of code that are written to verify the behavior or functionality of a specific part of the application.
+
+
+* describe: It is used to group together related test cases, providing a way to organize and structure your tests in a logical manner. By using `describe()`, you can create nested test suites that help improve readability and maintainability of your test code.
 */
