1b7f0a65f6c37b80763a69b9d2d37c2ace530740
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-19 22:34:04 +0330

    Testing User Interaction | State

--- Testing-Apps/src/components/Greeting.js
+++ Testing-Apps/src/components/Greeting.js
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+
 export default function Greeting() {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It's good to see you!</p>
+
+      {!changedText && <p>It's good to see you!</p>}
+      {changedText && <p>Testing changes was successful.</p>}
+
+      <button onClick={changeTextHandler}>Change Text</button>
     </div>
   );
 }
--- Testing-Apps/src/components/Greeting.test.js
+++ Testing-Apps/src/components/Greeting.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
-describe('<Greeting />', () => {
+describe('Greeting Component', () => {
   // test(description: what your test does, fn: testing codes )
-  test('renders Hello World as a text', () => {
+  test('renders "Hello World" as a text', () => {
     // Arrange: set up test data
     render(<Greeting />);
 
@@ -14,6 +15,45 @@ describe('<Greeting />', () => {
     const helloWorldEl = screen.getByText('Hello World', { exact: false });
 
     expect(helloWorldEl).toBeInTheDocument(); // toBeInTheDocument() is a matcher
+  });
+
+  test('renders "good to see you" if the button was NOT clicked', () => {
+    render(<Greeting />);
+
+    const greetingSentence = screen.getByText('good to see you', {
+      exact: false,
+    });
+    expect(greetingSentence).toBeInTheDocument();
+  });
+
+  test('renders "changes was successful" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    // Assert
+    const outputEl = screen.getByText('changes was successful', {
+      exact: false,
+    });
+    expect(outputEl).toBeInTheDocument();
+  });
+
+  test('does not renders "good to see you" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    // Assert
+    const outputEl = screen.queryByText('good to see you', {
+      exact: false,
+    });
+    expect(outputEl).toBeNull();
   });
 });
 
@@ -30,4 +70,16 @@ describe('<Greeting />', () => {
 
 
 * describe: It is used to group together related test cases, providing a way to organize and structure your tests in a logical manner. By using `describe()`, you can create nested test suites that help improve readability and maintainability of your test code.
+*/
+
+/* userEvent
+-  is a utility function provided by the Testing Library for React that allows developers to simulate user interactions with the components in their applications during testing. 
+
+- This function is particularly useful for writing integration tests that closely mimic how real users would interact with the UI elements.
+*/
+
+/* queryByText
+- if my expectation is that an element NOT be found in the document and "getByText" will throw an error if it's not found, this test could never pass
+
+- but queryByText will return "null" if the element is NOT found
 */
