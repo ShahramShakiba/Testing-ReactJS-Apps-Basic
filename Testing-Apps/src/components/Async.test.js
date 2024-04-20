8393014c9560675a8094a452eaea95ae8224b91a
ShahramShakiba <Shahramshakiba88@gmail.com>
2024-04-20 11:42:54 +0330

    Working With Mocks

--- Testing-Apps/src/components/Async.test.js
+++ Testing-Apps/src/components/Async.test.js
@@ -3,9 +3,30 @@ import Async from './Async';
 
 describe('Async Component', () => {
   test('should renders posts if request succeeds.', async () => {
+    window.fetch = jest.fn();
+    // set a value which this fetch fn should resolve to when it's being called
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: 'p1', title: 'First post' }],
+    });
+
     render(<Async />);
 
     const listItem = await screen.findAllByRole('listitem');
     expect(listItem).not.toHaveLength(0);
   });
 });
+
+/* Jest - mock function
+
+* Jest :
+  -  It provides a robust set of features for writing and running tests, including support for mocking functions and modules, asserting expectations, and generating code coverage reports. 
+  
+  - Jest simplifies the testing process by offering an intuitive API and seamless integration with React components.
+
+
+* mock function:
+  - Mock functions are placeholders for real functions that simulate their behavior during testing. 
+  
+  - By using mock functions, you can isolate the code under test and control its interactions with external dependencies, ensuring that your tests focus on specific functionality without relying on actual implementations.
+
+*/
